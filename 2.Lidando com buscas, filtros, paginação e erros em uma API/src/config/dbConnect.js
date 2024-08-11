f493b12260b7b4bab99523887c132f964e860447
Rafael Campos <93745489+rafahcampos@users.noreply.github.com>
2024-08-11 16:07:13 -0300

    Update dbConnect.js

--- 2.Lidando com buscas, filtros, paginação e erros em uma API/src/config/dbConnect.js
+++ 2.Lidando com buscas, filtros, paginação e erros em uma API/src/config/dbConnect.js
@@ -5,5 +5,3 @@ mongoose.connect(process.env.STRING_CONEXAO_BD);
 let db = mongoose.connection;
 
 export default db;
-
-//mongodb+srv://admin:admin2024@cluster0.lv4hvxu.mongodb.net/lanhouse?retryWrites=true&w=majority&appName=Cluster0
